7f57f093353251030e5bf37347ebcab157955ee7
Kwuffin <bestboykevin.bbg@gmail.com>
2021-02-24 17:16:07 +0100

    Perceptron with Learning Rule and error calculation.
    Perceptron initialization and training

---
+++ P2/perceptron.py
@@ -0,0 +1,86 @@
+from math import e
+
+
+def step(x):
+    """
+    Step function
+    :param x: any integer value
+    :return:
+    """
+    return int(x >= 0)
+
+
+def sigmoid(x):
+    """
+    Sigmoid function
+    :param x: any integer value
+    :return:
+    """
+    return 1/(1+e**(-x))
+
+
+class Perceptron:
+    def __init__(self, name: str, weights: [float], bias: float):
+        self.name = name
+        self.weights = weights
+        self.bias = bias
+        self.output = 0
+
+    # String function that prints useful attributes
+    def __str__(self):
+        return f"Perceptron '{self.name}':\nWeight(s): {self.weights}\nBias:      {self.bias}\n"
+
+    def calc(self, inputs: [int]) -> int:
+        """
+        Calculates the perceptron's output
+        :param inputs: Output values of the previous layer (or first set of inputs)
+        :return: Output of perceptron
+        """
+        # Error catching if length of lists is not the same
+        if len(inputs) == len(self.weights):
+            weighted = []  # List of all weighted inputs
+
+            # Weigh all inputs
+            for i in range(len(inputs)):
+                weighted_input = inputs[i] * self.weights[i]
+                weighted.append(weighted_input)
+
+            total = sum(weighted) + self.bias  # Add bias to sum of weighted inputs
+
+            return step(total)
+
+        # If there are not enough weights for inputs, or vice versa.
+        else:
+            raise Exception(f"Inputs and Weights do not have an equal amount of elements\n"
+                            f"Inputs:  {len(inputs)}\n"
+                            f"Weights: {len(self.weights)}")
+
+    def update(self, target: int, inputs: [int], learning_rate: float):
+        """
+        Perceptron Learning Rule, adjusts weights and bias
+        :param target: The correct answer
+        :param inputs: List
+        :param learning_rate:
+        """
+        # Executes the calc() function, to see what the output would be with given weights and biases
+        a = self.calc(inputs)
+
+        if a != target:
+            self.bias += learning_rate * (target - a) * 1
+
+            new_weights = []
+            for i in range(len(self.weights)):
+                new_weight = self.weights[i] + (learning_rate * (target - a) * inputs[i])
+                new_weights.append(new_weight)
+
+            self.weights = new_weights
+
+    def error(self, inputs: [[int]], targets: [int]):
+        errors = []
+        for i in range(len(inputs)):
+            a = self.calc(inputs[i])
+
+            error = targets[i] - a
+            errors.append(error)
+
+        return sum(errors) / len(errors)
---
+++ P2/perceptron_training.py
@@ -0,0 +1,64 @@
+from perceptron import Perceptron
+from random import randint  # Is used to initialize perceptron classes with random values
+from random import seed  # Is used to set the seed for the iris dataset
+from sklearn.datasets import load_iris
+
+
+def train_perceptron(gate, bit_combinations, targets):
+    epochs = 0
+    while True:
+        values = []
+        for i in range(len(bit_combinations)):
+            a = gate.calc(bit_combinations[i])
+            values.append(a)
+
+            gate.update(target=targets[i], inputs=bit_combinations[i], learning_rate=0.3)
+
+            epochs += 1
+
+        if values == targets:
+            print(f"Target outputs have been reached after {epochs} iteration(s).\n")
+            print(f"Final weights and bias:\n"
+                  f"{gate.__str__()}")
+            break
+
+        if epochs == 1000:
+            print(f"No correct weights and biases found within 4000 iterations for '{gate.name}'.")
+            break
+
+
+def and_gate_train():
+    bit_combinations = [[1, 1], [1, 0], [0, 1], [0, 0]]
+
+    and_targets = [0, 0, 0, 1]
+    and_gate = Perceptron("And gate", [randint(-5, 5), randint(-5, 5)], randint(-5, 5))
+
+    train_perceptron(and_gate, bit_combinations, and_targets)
+
+
+def xor_gate_train():
+    bit_combinations = [[1, 1], [1, 0], [0, 1], [0, 0]]
+
+    and_targets = [0, 1, 1, 0]
+    xor_gate = Perceptron("Xor gate", [randint(-5, 5), randint(-5, 5)], randint(-5, 5))
+
+    train_perceptron(xor_gate, bit_combinations, and_targets)
+
+
+def iris_train():
+    seed(1763456)
+    iris_perceptron = Perceptron("Iris", [randint(-5, 5), randint(-5, 5)], randint(-5, 5))
+    data = load_iris()
+    print(data['data'])
+
+
+def main():
+    print("====================| And-Gate: |====================")
+    and_gate_train()
+    print("====================| Xor-Gate: |====================")
+    xor_gate_train()
+    # iris_train()
+
+
+if __name__ == '__main__':
+    main()
