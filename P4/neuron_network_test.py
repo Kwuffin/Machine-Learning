f1fc6ff051434682d68c8316cc0693fc8d4def24
Kwuffin <bestboykevin.bbg@gmail.com>
2021-03-12 20:36:20 +0100

    Added xor gate

--- P4/neuron_network_test.py
+++ P4/neuron_network_test.py
@@ -6,18 +6,50 @@ from random import randint
 net1 = NeuronNetwork([NeuronLayer([Neuron("1", [0, 0], 0), Neuron("2", [-1, -1], -1)]),
                       NeuronLayer([Neuron("3", [1, 1], 1)])])
 
-and_port = NeuronNetwork([
-    NeuronLayer([
-        Neuron("And-gate", [-0.5, 0.5], 1.5)])])
 
-input_combinations = [[0, 0], [1, 0], [0, 1], [1, 1]]
-targets = [0, 0, 0, 1]
+input_combinations_2 = [[0, 0], [1, 0], [0, 1], [1, 1]]
+input_combinations_3 = [[0, 0, 0],
+                        [0, 0, 1],
+                        [0, 1, 0],
+                        [0, 1, 1],
+                        [1, 0, 0],
+                        [1, 0, 1],
+                        [1, 1, 0],
+                        [1, 1, 1]]
 
 
-def and_port_train(inputs, targets):
+def and_port_train(inputs):
+    targets = [0, 0, 0, 1]
+
+    and_port = NeuronNetwork([
+        NeuronLayer([
+            Neuron("And-gate", [randint(-1, 1), randint(-1, 1)], randint(-1, 1))])])
+
     iterations, errors, outputs = and_port.train(inputs, targets, 1)
+
     print([neuron.weights for layer in and_port.layers for neuron in layer.neurons])
     print(iterations, errors, outputs)
 
 
-and_port_train(input_combinations, targets)
+def xor_port_train(inputs):
+    targets = [0, 1, 1, 0]
+    xor_port = NeuronNetwork([
+        NeuronLayer([
+            Neuron("Nor gate", [randint(-1, 1), randint(-1, 1)], randint(-1, 1)),
+            Neuron("And gate", [randint(-1, 1), randint(-1, 1)], randint(-1, 1))
+        ]),
+        NeuronLayer([
+            Neuron("Nor gate", [randint(-1, 1), randint(-1, 1)], randint(-1, 1))
+        ])
+    ])
+
+    iterations, errors, outputs = xor_port.train(inputs, targets, 1)
+
+    layer = xor_port.layers[-1]
+
+    print([neuron.weights for neuron in layer.neurons])
+    print(iterations, errors, outputs)
+
+
+and_port_train(input_combinations_2)
+xor_port_train(input_combinations_2)
