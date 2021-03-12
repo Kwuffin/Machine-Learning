89898c8b6ea05782a5201c6818a66264d09821ec
Kwuffin <bestboykevin.bbg@gmail.com>
2021-03-12 20:04:36 +0100

    Updated documentation

--- P4/neuronNetwork.py
+++ P4/neuronNetwork.py
@@ -18,6 +18,12 @@ class NeuronNetwork:
         return inputs
 
     def update(self, inputs, learning_rate):
+        """
+        Updates all weights and biases
+        :param inputs: The training example
+        :param learning_rate:
+        :return:
+        """
         for layer in self.layers:
             inputs = layer.update(inputs, learning_rate)
 
@@ -33,26 +39,32 @@ class NeuronNetwork:
         # Stop at 4000 iterations
         while iterations < 4000:
             iterations += 1
-            outputs = []
+            outputs = []  # Outputs of all training examples
             for input_index, training_example in enumerate(inputs):
-                self.feed_forward(training_example)
-                errors = []
+                self.feed_forward(training_example)  # The feed forward makes sure all neurons have an error attribute
+                errors = []  # All errors in a list
+
                 for i, layer in enumerate(reversed(self.layers)):
+                    # If current layer is the output layer
                     if i == 0:
+                        # Calculate the output of every output neuron
                         for neuron in layer.neurons:
                             error = neuron.calc_error(training_example, targets[input_index])
                             errors.append(error)
                             outputs.append(neuron.output)
 
                     else:
+                        # Calculate the output of every hidden neuron
                         for j, neuron in enumerate(layer.neurons):
                             error = neuron.calc_error_hidden(self.layers[i], j)
                             errors.append(error)
                             outputs.append(neuron.output)
 
-                for e in errors:
+                for e in errors:  # If any error is below value
                     if e < 0.00000001:
                         return iterations, errors, outputs
+
+                # Update weights and biases
                 self.update(training_example, learning_rate)
 
         return iterations, errors, outputs
